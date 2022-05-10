f5d26b187727f3843f2ab33e5f5a52af261a880d
NicklasSpendler <nicklas.spendler@gmail.com>
2022-05-10 13:09:23 +0200

    Added Song Model

---
+++ DotnetExam/Models/Song.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetExam.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public Artist artist { get; set; }
+    }
+}
