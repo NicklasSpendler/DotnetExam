7460ad23d493fbaad229953833e757a3f7b897a9
Jev22 <50078304+Jev22@users.noreply.github.com>
2022-05-24 09:27:57 +0200

    Revert "fixed identityuser"
    This reverts commit fd53c25023e32cd2ec79e2eace3ea7fa7ca6ed39.

--- DotnetExam/Models/Comment.cs
+++ DotnetExam/Models/Comment.cs
@@ -8,7 +8,7 @@
         public int SongId { get; set; }
         public Song Song { get; set; }
         public string UserId { get; set; }
-        public IdentityUser? User { get; set; }
+        public int IdentityUser? { get; set; }
         
     }
 }
